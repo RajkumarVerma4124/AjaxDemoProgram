ec8737dd107579eb41c8a8148a6232f846bf640b
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-03-19 10:37:00 +0530

    [Raj] Added:- The code to test async nature of js

---
+++ js/AsyncTimer.js
@@ -0,0 +1,14 @@
+//Function to get the current time(UC1)
+function showTime() {
+    const date = new Date();
+    return date.getHours() + " Hrs:: " + date.getMinutes() + " Minutes:: " + date.getMilliseconds() + " MilliSeconds";
+}
+
+//Function to show the session expire after some time(UC1)
+function showSessionExpire() {
+    console.log("Activity B: Your Session Expired At :: " + showTime());
+}
+console.log("Activity A: Triggering Activity-B At :: " + showTime());
+setTimeout(showSessionExpire, 5000);
+console.log("Activity A: Triggering Activity-B At :: " + showTime() + " Will Execute After 5 Seconds");
+module.exports = { showTime }
